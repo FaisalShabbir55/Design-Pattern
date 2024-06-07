27428daf02e13e4d8c4386cffd2509a0f4775f5a
FaisalShabbir55 <116498207+FaisalShabbir55@users.noreply.github.com>
2024-06-07 19:28:15 +0500

    Strategy Pattern
    Adding a new Strategy Pattern in 7/6/24.

---
+++ Genaric Code of Strategy pattern.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+
+
+class Fighter
+{
+public:
+	Fighter(){}
+
+	virtual void weapon() = 0;
+
+
+	~Fighter(){}
+
+private:
+
+};
+
+class KnifeFighter : public Fighter
+{
+public:
+	KnifeFighter(){}
+
+	void weapon(){
+		cout << "Fighter fight with Knife" << endl;
+	}
+
+	~KnifeFighter(){}
+
+private:
+
+};
+
+
+class SwordFighter : public Fighter
+{
+public:
+	SwordFighter() {}
+
+	void weapon() {
+		cout << "Fighter fight with Sword" << endl;
+	}
+
+	~SwordFighter() {}
+
+private:
+
+};
+
+class GunFighter : public Fighter
+{
+public:
+	GunFighter(){}
+
+	void weapon() {
+		cout << "Fighter fight with Guns" << endl;
+	}
+
+
+	~GunFighter(){}
+
+private:
+
+};
+
+
+
+class Context
+{
+public:
+	Context(Fighter* obj):obj(obj){}
+
+	void setStrategy(Fighter* obj) {
+		this->obj = obj;
+	}
+
+	void selectWeapon() {
+		obj->weapon();
+
+	}
+
+	~Context(){}
+
+private:
+	Fighter* obj;
+};
+
+
+int main() {
+
+	KnifeFighter knifeFighter;
+	GunFighter gunFighter;
+	SwordFighter swordFighter;
+
+	Context context(&knifeFighter);
+
+	cout << "Default strategy:" << endl;
+	context.selectWeapon();
+
+
+	context.setStrategy(&gunFighter);
+
+	cout << "Updated strategy:" << endl;
+	context.selectWeapon();
+
+	context.setStrategy(&swordFighter);
+	cout << "New Strategy: " << endl;
+	context.selectWeapon();
+
+	return 0;
+}
+
+
+
